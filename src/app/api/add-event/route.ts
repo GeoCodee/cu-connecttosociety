af802fadbf7ab4bec4ac85dd12cd14a9cce0cb67
GeoffreyFornoles <geofornoles897@gmail.com>
2024-03-21 12:11:37 -0400

    added backend

---
+++ src/app/api/add-event/route.ts
@@ -0,0 +1,24 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const { userId } = auth();
+  const eventName = body.eventName;
+  const eventLocation = body.eventLocation;
+  const eventTime = body.eventTime;
+  const capacity = body.capacity;
+  //   const eventType = body.eventType;
+  //   const email;
+
+  try {
+    await sql`INSERT INTO Event(userid,eventname,eventlocation,eventtime) VALUES(${userId},${eventName},${eventLocation},${eventTime})`;
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+
+  const events = await sql`SELECT * FROM Event`;
+  return NextResponse.json({ events }, { status: 200 });
+}
