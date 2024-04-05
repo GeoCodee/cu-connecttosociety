cc8ce0145c58fafd278f9f95cd2a429b71079ac1
GeoffreyFornoles <geofornoles897@gmail.com>
2024-04-05 15:16:52 -0400

    improve UI for event (still needs work)

--- src/app/(content)/profile/page.tsx
+++ src/app/(content)/profile/page.tsx
@@ -1,6 +1,25 @@
 "use client";
-export default function Home() {
+
+import EventCard from "@/components/component/event-card";
+import { eventDetails } from "@/components/component/event-card";
+
+const testDetails: eventDetails = {
+  eventId: 1,
+  eventName: "Test Name",
+  eventDescription: "testDescription",
+  eventHost: "Geo",
+  eventLocation: "Missisauga",
+  eventDate: "2024-04-02",
+  eventStart: "10:00",
+  eventEnd: "12:00",
+  capacity: 2,
+};
+
+export default function Profile() {
   return (
-    <div className="h-screen overflow-hidden py-10 overscroll-none flex relative"></div>
+    <div className="flex flex-wrap gap-4">
+      <EventCard {...testDetails}></EventCard>
+      {/* <EventCard></EventCard> */}
+    </div>
   );
 }
---
+++ src/app/api/get-all-events/route.ts
@@ -0,0 +1,28 @@
+import { auth } from "@clerk/nextjs";
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { userId } = auth();
+
+  try {
+    //leave it here, this route will get outdated data from DBb without this line of code for some reason
+    let url: string = request.url;
+    // await sql`DROP TABLE IF EXISTS EVENT`;
+
+    // const result = await sql`
+    //           SELECT * FROM EVENT;
+    //         `;
+
+    // console.log(userId);
+
+    //only select events that have capaciy > 0
+    //only select events that doesn't have userId and eventId in event_particiption
+    const result = await sql`
+        SELECT * FROM EVENT
+      `;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
